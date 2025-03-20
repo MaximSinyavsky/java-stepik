7c96c592bfb732712e207f53a5ad4459e4a2b284
MaximSinyavsky <razarsinet1991@gmail.com>
2025-03-20 11:56:40 +0200

    updated WithdrawalOfStrangers

--- WithdrawalOfStrangers.java
+++ WithdrawalOfStrangers.java
@@ -4,24 +4,22 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberOfStrangers = scanner.nextInt();
-        scanner.nextLine();
+        String input = scanner.nextLine();
+        String[] parts = input.split("\\s+");
+        int numberOfStrangers = Integer.parseInt(parts[0]);
         if (numberOfStrangers == 0) {
             System.out.println("Оу... Похоже здесь никого...");
         } else if (numberOfStrangers < 0) {
             System.out.println("Серьезно? А что так негативно?");
         } else {
-            String namesInput = scanner.nextLine();
-            String[] namesOfStrangers = namesInput.split(" ");
-            if (namesOfStrangers.length == numberOfStrangers) {
-                for (String name : namesOfStrangers) {
-                    System.out.println("Привет, " + name);
-                }
-            } else {
+            if (parts.length - 1 != numberOfStrangers) {
                 System.out.println("Количество имен не соответствует количеству незнакомцев.");
+            } else {
+                for (int i = 1; i < parts.length; i++) {
+                    System.out.println("Привет, " + parts[i]);
+                }
             }
         }
-
         scanner.close();
     }
 }
