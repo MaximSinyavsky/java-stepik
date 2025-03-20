61cb846ad030abcd06fe26aa5b0e097351d5ca3d
MaximSinyavsky <razarsinet1991@gmail.com>
2025-03-20 15:31:14 +0200

    update SqrtEquation

--- SqrtEquation.java
+++ SqrtEquation.java
@@ -8,20 +8,20 @@ public class Main {
         double b = scanner.nextInt();
         double c = scanner.nextInt();
         double d = b * b - 4 * a * c;
-        if (d>0){
+        if (d > 0) {
             double sqrtD = Math.sqrt(d);
-            double x1 = (-b -sqrtD)/(2*a);
-            double x2 = (-b + sqrtD) /(2*a);
-            if (x1>x2){
+            double x1 = (-b - sqrtD) / (2 * a);
+            double x2 = (-b + sqrtD) / (2 * a);
+            if (x1 > x2) {
                 double temp = x1;
                 x1 = x2;
                 x2 = temp;
             }
             System.out.println(x1 + " " + x2);
-        }else if (d<0){
-            double x = -b / (2*a);
+        } else if (d == 0) {
+            double x = -b / (2 * a);
             System.out.println(x);
-        }else {
+        } else {
             System.out.println("без корней");
         }
         scanner.close();
